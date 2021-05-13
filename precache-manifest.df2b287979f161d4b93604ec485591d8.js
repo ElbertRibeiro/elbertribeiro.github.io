79319276c9e3becee28f821acb5ed3ee1c23f1fc
ElbertRibeiro <elbertprofissional@gmail.com>
2021-05-13 19:33:05 -0300

    Updates

---
+++ precache-manifest.df2b287979f161d4b93604ec485591d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414780177243020757d3e1eeaa71e9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ebcc56025a8745ae261",
+    "url": "/static/js/2.07fb019f.chunk.js"
+  },
+  {
+    "revision": "696b6c596c38de00c1bba12d4d6df163",
+    "url": "/static/js/2.07fb019f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec724ff9c892209c9bf",
+    "url": "/static/js/main.7f126bbd.chunk.js"
+  },
+  {
+    "revision": "10a1ebe435e5e83d06e3",
+    "url": "/static/js/runtime-main.e1295efb.js"
+  },
+  {
+    "revision": "746eb4dad5b6db80049ff01604e29f7c",
+    "url": "/static/media/To-do.746eb4da.png"
+  },
+  {
+    "revision": "244e736bb85648eee2b10267088da7ac",
+    "url": "/static/media/challenge.244e736b.jpg"
+  },
+  {
+    "revision": "f73387e0451db0e4ac358e2e3fb6a100",
+    "url": "/static/media/chatbot.f73387e0.png"
+  },
+  {
+    "revision": "969c65f2f01017aa2ec311de26fa49c9",
+    "url": "/static/media/curriculo.969c65f2.pdf"
+  },
+  {
+    "revision": "43d0e2cc87e1bb9971092a8799e08e5b",
+    "url": "/static/media/dev.finance.43d0e2cc.png"
+  },
+  {
+    "revision": "b0171267284b1b1e9414c7a2ceae9683",
+    "url": "/static/media/headshot.b0171267.jpg"
+  },
+  {
+    "revision": "71c7550353a669a57a7584ddbaecf8dd",
+    "url": "/static/media/spiderman.71c75503.png"
+  }
+]);
